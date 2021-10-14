4f0894b0bea9a445265832d03724a2e5a15e5713
jhs851 <jhs851@naver.com>
2021-10-14 15:27:54 +0900

    [fix] @react-native-firebase 테스트 환경

--- tests/mocks.ts
+++ tests/mocks.ts
@@ -4,4 +4,12 @@ jest
   .mock('@react-native-async-storage/async-storage', () =>
     require('@react-native-async-storage/async-storage/jest/async-storage-mock'),
   )
+  .mock('@react-native-firebase/analytics', () => {
+    return () => ({
+      logEvent: jest.fn(),
+      setUserProperties: jest.fn(),
+      setUserId: jest.fn(),
+      setCurrentScreen: jest.fn(),
+    });
+  })
   .doMock('recoil', () => require('recoil/native/recoil'));
