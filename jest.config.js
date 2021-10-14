0151dae8a66ba7c0d25a3c527cf0ae2b73b26073
jhs851 <jhs851@naver.com>
2021-10-14 15:27:37 +0900

    [fix] recoil 테스트 환경

--- jest.config.js
+++ jest.config.js
@@ -172,7 +172,9 @@ module.exports = {
   // transform: undefined,
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
-  transformIgnorePatterns: ['node_modules/(?!@react-native|react-native)'],
+  transformIgnorePatterns: [
+    'node_modules/(?!@react-native|react-native|recoil)',
+  ],
 
   // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
   // unmockedModulePathPatterns: undefined,
--- tests/mocks.ts
+++ tests/mocks.ts
@@ -1,5 +1,7 @@
 export {};
 
-jest.mock('@react-native-async-storage/async-storage', () =>
-  require('@react-native-async-storage/async-storage/jest/async-storage-mock'),
-);
+jest
+  .mock('@react-native-async-storage/async-storage', () =>
+    require('@react-native-async-storage/async-storage/jest/async-storage-mock'),
+  )
+  .doMock('recoil', () => require('recoil/native/recoil'));
