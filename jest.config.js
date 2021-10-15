4e5762096899529c8c1dd50d5ad1fed770be5df6
jhs851 <jhs851@naver.com>
2021-10-15 17:55:13 +0900

    [feat] MaterialInput 컴포넌트
    #42

--- jest.config.js
+++ jest.config.js
@@ -176,7 +176,7 @@ module.exports = {
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   transformIgnorePatterns: [
-    'node_modules/(?!@react-native|react-native|recoil)',
+    'node_modules/(?!@react-native|react-native|recoil|rn-material-ui-textfield)',
   ],
 
   // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
---
+++ src/components/MaterialInput.tsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import { TextField, TextFieldProps } from 'rn-material-ui-textfield';
+import { Icon, useTheme } from 'react-native-elements';
+import {
+  Animated,
+  NativeSyntheticEvent,
+  TextInputChangeEventData,
+  TextInputFocusEventData,
+} from 'react-native';
+
+type P = TextFieldProps & {
+  onClear?: () => void;
+};
+
+const MaterialInput: React.ForwardRefRenderFunction<TextField, P> = (
+  props,
+  ref,
+) => {
+  const {
+    theme: { colors },
+  } = useTheme();
+  const fadeAnimation = new Animated.Value(0);
+  const fade = (focus: boolean) =>
+    Animated.timing(fadeAnimation, {
+      toValue: focus ? 1 : 0,
+      duration: 225,
+      useNativeDriver: true,
+    }).start();
+  const onFocus = (event: NativeSyntheticEvent<TextInputFocusEventData>) => {
+    fade(true);
+
+    if (props.onFocus) {
+      props.onFocus(event);
+    }
+  };
+  const onBlur = (event: NativeSyntheticEvent<TextInputChangeEventData>) => {
+    fade(false);
+
+    if (props.onBlur) {
+      props.onBlur(event);
+    }
+  };
+  const renderRightAccessory = () => (
+    <Animated.View style={{ opacity: fadeAnimation }}>
+      <Icon
+        testID="icon"
+        onPress={props.onClear}
+        name="close-circle"
+        type="ionicon"
+        color={colors?.grey4}
+      />
+    </Animated.View>
+  );
+
+  return (
+    <TextField
+      ref={ref}
+      textColor={colors?.black}
+      labelFontSize={14}
+      lineWidth={1}
+      activeLineWidth={1}
+      disabledLineWidth={1}
+      tintColor={colors?.grey3}
+      baseColor={colors?.grey3}
+      errorColor={colors?.error}
+      disabledLineType="solid"
+      renderRightAccessory={renderRightAccessory}
+      {...props}
+      onFocus={onFocus}
+      onBlur={onBlur}
+    />
+  );
+};
+
+export default React.forwardRef(MaterialInput);
