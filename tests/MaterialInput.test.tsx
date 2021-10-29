a68f4f7093f3170a75432433a59537de853a0f2b
jhs851 <jhs851@naver.com>
2021-10-29 17:35:08 +0900

    [feat] MaterialInput 컴포넌트 테스트
    #71

---
+++ tests/MaterialInput.test.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { render } from '@testing-library/react-native';
+import MaterialInput from '@src/components/MaterialInput';
+import { wrapper } from '@tests/functions';
+
+describe('MaterialInput 컴포넌트', () => {
+  const onChange = jest.fn();
+
+  it('렌더링이 올바르게 된다', () => {
+    const { toJSON } = render(
+      <MaterialInput onChange={onChange} label="이름" />,
+      { wrapper },
+    );
+
+    expect(toJSON()).toMatchSnapshot();
+  });
+});
