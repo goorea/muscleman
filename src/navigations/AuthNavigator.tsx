d0f5703b45582e58710c9a5300e09f20158d7e39
jhs851 <jhs851@naver.com>
2021-10-31 15:44:20 +0900

    [feat] RegisterNavigation 생성
    #40

--- src/navigations/AuthNavigator.tsx
+++ src/navigations/AuthNavigator.tsx
@@ -4,24 +4,39 @@ import {
   NativeStackScreenProps,
 } from '@react-navigation/native-stack';
 import LoginScreen from '@src/screens/LoginScreen';
-import RegisterScreen from '@src/screens/RegisterScreen';
-import { RootStackParamList } from '@src/types/navigation';
-import { StackNavigatorDefaultScreenOptions } from '@src/navigations/StackNavigatorDefaultScreenOptions';
+import { AuthStackParamList, RootStackParamList } from '@src/types/navigation';
+import { StackNavigatorDefaultScreenOptions } from './StackNavigatorDefaultScreenOptions';
 import SuccessModalScreen from '@src/screens/SuccessModalScreen';
+import RegisterNavigator from './RegisterNavigator';
+import { RouteGroupConfig } from '@react-navigation/native';
 
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator<AuthStackParamList>();
 
-type P = NativeStackScreenProps<RootStackParamList, 'Main'> & {};
+type P = NativeStackScreenProps<RootStackParamList, 'Auth'> & {};
 
-const AuthNavigator: React.FC<P> = () => (
-  <Stack.Navigator screenOptions={StackNavigatorDefaultScreenOptions}>
-    <Stack.Screen name="Login" component={LoginScreen} />
-    <Stack.Screen name="Register" component={RegisterScreen} />
+const AuthNavigator: React.FC<P> = () => {
+  const registerOptions = { headerShown: false };
+  const modalScreenOptions: RouteGroupConfig<
+    AuthStackParamList,
+    {}
+  >['screenOptions'] = { headerShown: false, presentation: 'modal' };
 
-    <Stack.Group screenOptions={{ headerShown: false, presentation: 'modal' }}>
-      <Stack.Screen name="SuccessModal" component={SuccessModalScreen} />
-    </Stack.Group>
-  </Stack.Navigator>
-);
+  return (
+    <Stack.Navigator
+      initialRouteName="Register"
+      screenOptions={StackNavigatorDefaultScreenOptions}>
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen
+        name="Register"
+        options={registerOptions}
+        component={RegisterNavigator}
+      />
+
+      <Stack.Group screenOptions={modalScreenOptions}>
+        <Stack.Screen name="SuccessModal" component={SuccessModalScreen} />
+      </Stack.Group>
+    </Stack.Navigator>
+  );
+};
 
 export default AuthNavigator;
