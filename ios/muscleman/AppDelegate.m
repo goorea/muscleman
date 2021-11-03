d32332136620798286c4efb50a76d736f8d89b4f
jhs851 <jhs851@naver.com>
2021-11-03 23:51:28 +0900

    [feat] ios LaunchScreen 변경
    #37

--- ios/muscleman/AppDelegate.m
+++ ios/muscleman/AppDelegate.m
@@ -13,6 +13,7 @@
 #import <FlipperKitReactPlugin/FlipperKitReactPlugin.h>
 
 #import <Firebase.h>
+#import "RNSplashScreen.h"
 
 static void InitializeFlipper(UIApplication *application) {
   FlipperClient *client = [FlipperClient sharedClient];
@@ -51,6 +52,7 @@ static void InitializeFlipper(UIApplication *application) {
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  [RNSplashScreen show];
   return YES;
 }
 
