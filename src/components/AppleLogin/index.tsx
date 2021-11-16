94cfbb6271bf904f4d2e5118729cd3d9f21e2604
jhs851 <jhs851@naver.com>
2021-11-17 06:53:10 +0900

    [feat] SocialIcon 컴포넌트 생성
    #90

--- src/components/AppleLogin/index.tsx
+++ src/components/AppleLogin/index.tsx
@@ -1,23 +1,20 @@
-import React, { useCallback } from 'react';
+import React, { ReactElement, useCallback, useMemo } from 'react';
+
+import SocialIcon from '@src/components/SocialIcon';
+import { SocialProvider } from '@src/types/graphql';
 
 import { SNSButton } from './styled';
 
 type P = {};
 
 const AppleLogin: React.FC<P> = () => {
+  const node = useMemo<ReactElement>(
+    () => <SocialIcon provider={SocialProvider.Apple} />,
+    [],
+  );
   const onAppleLogin = useCallback(() => {}, []);
 
-  return (
-    <SNSButton
-      icon={{
-        name: 'apple',
-        type: 'font-awesome-5',
-        color: 'background',
-      }}
-      onPress={onAppleLogin}
-      color="foreground"
-    />
-  );
+  return <SNSButton node={node} onPress={onAppleLogin} color="foreground" />;
 };
 
 export default AppleLogin;
