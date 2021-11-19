eafb889f8fae5a354c8acc629c9af94dc02905f9
jhs851 <jhs851@naver.com>
2021-11-19 13:57:09 +0900

    [test] PlansScreen 컴포넌트 테스트
    #92

---
+++ tests/PlansScreen.test.tsx
@@ -0,0 +1,41 @@
+import { navigationMock, navigationNavigateMock } from '@mocks/navigationMocks';
+import { act, fireEvent, render } from '@testing-library/react-native';
+import dayjs from 'dayjs';
+import React from 'react';
+
+import PlansScreen from '@src/screens/PlansScreen';
+import { wrapper } from '@tests/functions';
+
+describe('PlansScreen 컴포넌트', () => {
+  const rendered = () =>
+    render(
+      <PlansScreen
+        navigation={navigationMock}
+        route={{
+          key: '',
+          name: 'Plans',
+        }}
+      />,
+      { wrapper },
+    );
+
+  it('렌더링이 올바르게 된다', () => {
+    const { queryByText } = rendered();
+
+    expect(queryByText(dayjs().format('YYYY년 MMMM'))).not.toBeNull();
+    expect(queryByText('오늘의 운동 계획하기')).not.toBeNull();
+  });
+
+  it('오늘의 운동 계획하기 버튼을 누르면 EditPlan으로 이동한다', async () => {
+    const { getByText } = rendered();
+
+    await act(async () => fireEvent.press(getByText('오늘의 운동 계획하기')));
+
+    expect(navigationNavigateMock).toHaveBeenCalledWith('Planning', {
+      screen: 'EditPlan',
+      params: {
+        plannedAt: dayjs().format('YYYY-MM-DD'),
+      },
+    });
+  });
+});
