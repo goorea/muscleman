a462f2e44082ff101e8f01e370ed3779c22221fe
hyjeong49 <hyjeong49@coupang.com>
2022-04-05 10:34:04 +0900

    [feat] SelectPlans 컴포넌트 생성
    #92

---
+++ src/components/SelectPlans/hooks/useSelect.ts
@@ -0,0 +1,35 @@
+import { useCallback } from 'react';
+import { useRecoilState } from 'recoil';
+
+import { selectedPlans as selectedPlansAtom } from '@src/components/PreviousPlansModal/recoils';
+import { Plan } from '@src/types/graphql';
+
+const useSelect = (
+  plans: Plan[],
+): {
+  selected: boolean;
+  onToggleSelect: () => void;
+} => {
+  const [selectedPlans, setSelectedPlans] =
+    useRecoilState<Plan[]>(selectedPlansAtom);
+  const selectedPlansIds = selectedPlans.map(({ _id }) => _id);
+  const selected = plans.every(({ _id }) => selectedPlansIds.includes(_id));
+
+  const onToggleSelect = useCallback(() => {
+    const plansIds = plans.map(({ _id }) => _id);
+    setSelectedPlans(prevSelectedPlans =>
+      selected
+        ? prevSelectedPlans.filter(({ _id }) => !plansIds.includes(_id))
+        : prevSelectedPlans.concat(
+            plans.filter(({ _id }) => !selectedPlansIds.includes(_id)),
+          ),
+    );
+  }, [plans, selected, selectedPlansIds, setSelectedPlans]);
+
+  return {
+    selected,
+    onToggleSelect,
+  };
+};
+
+export default useSelect;
