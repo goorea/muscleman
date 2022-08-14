05b341edca852c23143c7a1d6edd8efa0fa42a81
jhs851 <jhs851@naver.com>
2022-08-14 22:06:13 +0900

    [feat] error-report
    #104

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { ApolloProvider } from '@apollo/client';
+import { SENTRY_DSN } from '@env';
+import * as Sentry from '@sentry/react-native';
 import React from 'react';
 import { RecoilRoot } from 'recoil';
 
@@ -9,6 +11,11 @@ import RecoilProvider from '@src/contexts/RecoilProvider';
 import ThemeProvider from '@src/contexts/ThemeProvider';
 import AppNavigator from '@src/navigations/AppNavigator';
 import NavigationContainer from '@src/navigations/NavigationContainer';
+
+Sentry.init({
+  dsn: SENTRY_DSN,
+  tracesSampleRate: 1.0,
+});
 
 const App: React.FC = () => (
   <ThemeProvider>
@@ -26,4 +33,4 @@ const App: React.FC = () => (
   </ThemeProvider>
 );
 
-export default App;
+export default Sentry.wrap(App);
